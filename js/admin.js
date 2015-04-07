4f37bc2af18b12d633e0a642ac9c2252e29b4318
Ahmad Game <ahmad.game@lundalogik.se>
2015-04-07 13:51:35 +0200

    register user in admin

--- js/admin.js
+++ js/admin.js
@@ -78,13 +78,78 @@ MK.vm.Admin = function () {
         });
     }
     //****************************************
-    // addQuestion
+    // addUser
     //****************************************
     var addUser = ko.observable(false);
+    var userName = ko.observable(""),
+        userEmail = ko.observable(""),
+        userPassword = ko.observable("");
+
+    function register() {
+        var user = new Parse.User();
+        user.set("username", userEmail());
+        user.set("password", userPassword());
+        user.set("email", userEmail());
+        user.set("name", userName());
+
+        user.signUp(null, {
+          success: function(user) {
+            alert("Success!")
+          },
+          error: function(user, error) {
+            errorAlert("Error: " + error.code + " " + error.message);
+          }
+        });
+    }
+
+    function initPopover(){
+        //minimum 8 characters
+        var bad = /(?=.{8,}).*/;
+        //Alpha Numeric plus minimum 8
+        var good = /^(?=\S*?[a-z])(?=\S*?[0-9])\S{8,}$/;
+        //Must contain at least one upper case letter, one lower case letter and (one number OR one special char).
+        var better = /^(?=\S*?[A-Z])(?=\S*?[a-z])((?=\S*?[0-9])|(?=\S*?[^\w\*]))\S{8,}$/;
+        //Must contain at least one upper case letter, one lower case letter and (one number AND one special char).
+        var best = /^(?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?=\S*?[^\w\*])\S{8,}$/;
+
+        $('#password').on('keyup', function () {
+            var password = $(this);
+            var pass = password.val();
+            var passLabel = $('[for="password"]');
+            var stength = 'Weak';
+            var pclass = 'danger';
+            if (best.test(pass) == true) {
+                stength = 'Very Strong';
+                pclass = 'success';
+            } else if (better.test(pass) == true) {
+                stength = 'Strong';
+                pclass = 'warning';
+            } else if (good.test(pass) == true) {
+                stength = 'Almost Strong';
+                pclass = 'warning';
+            } else if (bad.test(pass) == true) {
+                stength = 'Weak';
+            } else {
+                stength = 'Very Weak';
+            }
+
+            var popover = password.attr('data-content', stength).data('bs.popover');
+            popover.setContent();
+            popover.$tip.addClass(popover.options.placement).removeClass('danger success info warning primary').addClass(pclass);
+
+        });
+
+        $('input[data-toggle="popover"]').popover({
+            placement: 'top',
+            trigger: 'focus'
+        });
+    }
+
     //****************************************
     // viewUsers
     //****************************************
     var viewUsers = ko.observable(false);
+
     //****************************************
     // viewQuestions
     //****************************************
@@ -114,6 +179,12 @@ MK.vm.Admin = function () {
         save: save,
 
         addUser: addUser,
+        userName: userName,
+        userEmail: userEmail,
+        userPassword: userPassword,
+        register: register,
+        initPopover: initPopover,
+
         viewQuestions: viewQuestions,
         viewUsers: viewUsers
     }
